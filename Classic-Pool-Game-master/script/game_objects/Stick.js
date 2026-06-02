91bbdf7f43e9ffe4214ceb337a43624cd611d8a7
Matheus Felipe <matheusfelipem336@gmail.com>
2026-06-02 11:29:46 -0300

    Feature 1: Alteração da mecânica de tacada

--- Classic-Pool-Game-master/script/game_objects/Stick.js
+++ Classic-Pool-Game-master/script/game_objects/Stick.js
@@ -1,15 +1,67 @@
 "use strict";
 
-function Stick(position){
-    this.position = position;
-    this.origin = new Vector2(970,11);
-    this.shotOrigin = new Vector2(950,11);
-    this.shooting = false;
-    this.visible = true;
-    this.rotation = 0;
-    this.power = 0;
-    this.trackMouse = true;
+function Stick(position) {
+  this.position = position;
+  this.origin = new Vector2(970, 11);
+  this.shotOrigin = new Vector2(950, 11);
+  this.shooting = false;
+  this.visible = true;
+  this.rotation = 0;
+  this.power = 0;
+  this.trackMouse = true;
+  // Variáveis novas para controle de carregamento da força
+  this.charging = false;
+  this.powerDirection = 1;
 }
+
+/*
+ **
+ ** Feature 1: Alteração da mecânica de carregamento da força
+ ** Ao teclar W é pressionada, a força começa a aumentar gradualmente, oscilando entre 0 e 100.
+ ** O jogador pode soltar a tecla W a qualquer momento para disparar a bola com a força atual.
+ **
+ ** Função handleInput() refeita:
+ */
+
+Stick.prototype.handleInput = function (delta) {
+  if (AI_ON && Game.policy.turn === AI_PLAYER_NUM) return;
+
+  if (Game.policy.turnPlayed) return;
+
+  if (Keyboard.down(Keys.W) && KEYBOARD_INPUT_ON) {
+    this.charging = true;
+
+    // Velocidade da oscilação da força do taco
+    this.power += this.powerDirection * 0.4;
+
+    if (this.power >= 100) {
+      this.power = 100;
+      this.powerDirection = -1;
+    }
+
+    if (this.power <= 0) {
+      this.power = 0;
+      this.powerDirection = 1;
+    }
+
+    this.origin.x = 990 - this.power * -0.47;
+  } else if (this.charging) {
+    this.charging = false;
+
+    if (this.power > 0) {
+      this.shoot(this.power, this.rotation);
+    }
+  }
+
+  if (this.trackMouse) {
+    var opposite = Mouse.position.y - this.position.y;
+    var adjacent = Mouse.position.x - this.position.x;
+    this.rotation = Math.atan2(opposite, adjacent);
+  }
+};
+
+/*
+** Função handeInput() antiga:
 
 Stick.prototype.handleInput = function (delta) {
 
@@ -51,14 +103,14 @@ Stick.prototype.handleInput = function (delta) {
       this.rotation = Math.atan2(opposite, adjacent);
     }
 };
-
-Stick.prototype.shoot = function(power, rotation){
+*/
+Stick.prototype.shoot = function (power, rotation) {
   this.power = power;
   this.rotation = rotation;
 
-  if(Game.sound && SOUND_ON){
+  if (Game.sound && SOUND_ON) {
     var strike = sounds.strike.cloneNode(true);
-    strike.volume = (this.power/(10))<1?(this.power/(10)):1;
+    strike.volume = this.power / 10 < 1 ? this.power / 10 : 1;
     strike.play();
   }
   Game.policy.turnPlayed = true;
@@ -67,25 +119,34 @@ Stick.prototype.shoot = function(power, rotation){
 
   Game.gameWorld.whiteBall.shoot(this.power, this.rotation);
   var stick = this;
-  setTimeout(function(){stick.visible = false;}, 500);
-}
-
-Stick.prototype.update = function(){
-  if(this.shooting && !Game.gameWorld.whiteBall.moving)
-    this.reset();
+  setTimeout(function () {
+    stick.visible = false;
+  }, 500);
 };
 
-Stick.prototype.reset = function(){
+Stick.prototype.update = function () {
+  if (this.shooting && !Game.gameWorld.whiteBall.moving) this.reset();
+};
+
+Stick.prototype.reset = function () {
   this.position.x = Game.gameWorld.whiteBall.position.x;
   this.position.y = Game.gameWorld.whiteBall.position.y;
-	this.origin = new Vector2(970,11);
+  this.origin = new Vector2(970, 11);
   this.shooting = false;
   this.visible = true;
-	this.power = 0;
+  this.power = 0;
+  // Variáveis novas para controle de carregamento da força
+  this.powerDirection = 1;
+  this.charging = false;
 };
 
 Stick.prototype.draw = function () {
-  if(!this.visible)
-    return;
-  Canvas2D.drawImage(sprites.stick, this.position,this.rotation,1, this.origin);
+  if (!this.visible) return;
+  Canvas2D.drawImage(
+    sprites.stick,
+    this.position,
+    this.rotation,
+    1,
+    this.origin,
+  );
 };
